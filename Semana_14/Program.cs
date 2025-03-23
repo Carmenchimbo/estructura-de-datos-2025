e0cddd9d236129510d0036bc123cae218050916b
Carmenchimbo <159070922+Carmenchimbo@users.noreply.github.com>
2025-03-23 16:09:42 -0500

    Update Nodo.cs

--- Semana_14/Program.cs
+++ Semana_14/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+//proponer un ejemplo de árboles binarios, con tipos de datos primitivos 
+//(enteros, cadenas, etc.) o de objetos a su elección, para demostrar las
+//principales operaciones contenidas en las diapositivas de esta semana.
+//La aplicación deberá contenerun menú para mostrar cada una de las operaciones
+//y el registro de datos dentro del árbol, se lo realizará mediante teclado.
 
 // Clase que representa un nodo del árbol binario
 class Node
